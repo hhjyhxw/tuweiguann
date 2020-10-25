3c4e32ed55ee63edd750be951971cf0a0e2d7782
hhjyhxw <512162086@qq.com>
2020-10-25 17:46:01 +0800

    add

--- src/main/resources/statics/js/modules/small/smallorder.js
+++ src/main/resources/statics/js/modules/small/smallorder.js
@@ -3,10 +3,11 @@ $(function () {
         url: baseURL + 'small/smallorder/list',
         datatype: "json",
         colModel: [			
-			{ label: 'id', name: 'id', index: 'id', width: 50, key: true },
-			{ label: '下单渠道', name: 'channel', index: 'channel', width: 80 }, 			
-			{ label: '订单号', name: 'orderNo', index: 'order_no', width: 80 }, 			
-			{ label: '用户昵称', name: 'user.nickname', index: 'user_id', width: 80 },
+			/*{ label: 'id', name: 'id', index: 'id', width: 50, key: true },*/
+			/*{ label: '下单渠道', name: 'channel', index: 'channel', width: 80 }, 			*/
+			{ label: '订单号', name: 'orderNo', index: 'order_no', width: 80 },
+			{ label: '用户id', name: 'userId', index: 'user_id', width: 80 },
+			{ label: '用户昵称', name: 'user.nickname', index: 'nickname', width: 80 },
 			{ label: '店铺名称', name: 'shop.shopName', index: 'supplier_id', width: 80 },
             { label: '订单状态', name: 'orderStatus', width: 60, formatter: function(value, options, row){
                     return value === 0 ?
@@ -19,25 +20,28 @@ $(function () {
                         '<span class="label label-danger">未支付</span>' :
                         (value===1?'<span class="label label-success">支付中</span>':'<span class="label label-success">已支付</span>');
                 }},
-            { label: '退款状态', name: 'refundStatus', width: 60, formatter: function(value, options, row){
+           /* { label: '退款状态', name: 'refundStatus', width: 60, formatter: function(value, options, row){
                     return value === 0 ?
                         '<span class="label label-danger">未退款</span>' :
                         (value===1?'<span class="label label-success">退款中</span>':'<span class="label label-success">已退款</span>');
-                }},
+                }},*/
             { label: '发货状态', name: 'shipStatus', width: 60, formatter: function(value, options, row){
                     return value === 0 ?
                         '<span class="label label-danger">未发货</span>' :
                         (value===1?'<span class="label label-success">发货中</span>':'<span class="label label-success">已配送</span>');
                 }},
-			{ label: '运费(元)', name: 'freightPrice', index: 'freight_price', width: 80 },
-			{ label: '代金券优惠价(元)', name: 'couponPrice', index: 'coupon_price', width: 80 },
-			{ label: '实付订单金额(元)', name: 'actualPrice', index: 'actual_price', width: 80 },
-			{ label: '支付金额(元)', name: 'payPrice', index: 'pay_price', width: 80 },
-			{ label: '支付流水', name: 'payId', index: 'pay_id', width: 80 },
-			{ label: '支付渠道名称', name: 'payChannel', index: 'pay_channel', width: 80 }, 			
-			{ label: '支付时间', name: 'payTime', index: 'pay_time', width: 80 },
+			/*{ label: '运费(元)', name: 'freightPrice', index: 'freight_price', width: 80 },
+			{ label: '代金券优惠价(元)', name: 'couponPrice', index: 'coupon_price', width: 80 },*/
+			{ label: '订单金额(元)', name: 'actualPrice', index: 'actual_price', width: 80 , formatter: function(value, options, row){
+			       return '￥'+value;
+            }},
+		/*	{ label: '支付金额(元)', name: 'payPrice', index: 'pay_price', width: 80 },
+			{ label: '支付流水', name: 'payId', index: 'pay_id', width: 80 },*/
+			{ label: '支付渠道名', name: 'payChannel', index: 'pay_channel', width: 80 },
+			{ label: '创建时间', name: 'createTime', index: 'create_time', width: 80 },
+			/*{ label: '支付时间', name: 'payTime', index: 'pay_time', width: 80 },
 			{ label: '发货时间', name: 'shipTime', index: 'ship_time', width: 80 },
-			{ label: '确认收货时间', name: 'confirmTime', index: 'confirm_time', width: 80 }
+			{ label: '确认收货时间', name: 'confirmTime', index: 'confirm_time', width: 80 }*/
 
         ],
 		viewrecords: true,
